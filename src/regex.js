15fa9ce0803d451b4f040851a764dadb0616f672
mayday <mdalil@student.42.fr>
2020-01-18 13:50:00 +0100

    babel publish script

--- src/regex.js
+++ src/regex.js
@@ -1,3 +1,4 @@
 export const REGEX_NAME = /^(?=.{3,32}$)[a-zA-Z]+(?:['_.\-\s][a-zA-Z]+)*$/;
 export const REGEX_MAIL = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
 export const REGEX_PASSWORD = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9\-]{8,})$/;
+export const REGEX_USERNAME = /^[a-zA-Z0-9]([._](?![._])|[a-zA-Z0-9]){6,18}[a-zA-Z0-9]$/;
